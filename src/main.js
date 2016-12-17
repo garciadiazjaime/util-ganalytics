bda0acd1755096eb93a05dbfe7e956de707a92c6
jaime <garciadiazjaime@gmail.com>
2016-12-17 14:37:23 -0600

    include googleId

--- src/main.js
+++ src/main.js
@@ -1,24 +1,25 @@
 export default class GaUtil {
 
-  constructor(flag) {
+  constructor(googleId, flag) {
+    this.googleId = googleId;
     this.enable = !!flag;
   }
 
   init(id) {
-    if (this.enable) {
+    if (this.googleId && this.enable) {
       /*eslint-disable */
       (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
       (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
       m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-      ga('create', id, 'auto');
+      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+      ga('create', this.googleId, 'auto');
       ga('send', 'pageview');
       /*eslint-enable */
     }
   }
 
   sendEvent(eventCategory, eventAction, eventLabel, eventValue) {
-    if (this.enable) {
+    if (this.enable && eventCategory && eventAction && eventLabel) {
       /*eslint-disable */
       ga('send', 'event', eventCategory, eventAction, eventLabel, eventValue);
       /*eslint-enable */
